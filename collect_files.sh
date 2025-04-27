c6cc08df1b9e5a28ac597f7d3dcd17f66f0494ec
blablash0 <anviplotnikova@edu.hse.ru>
2025-04-27 19:16:34 +0700

    Add collect_files.sh

---
+++ collect_files.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+if ["$#" - lt 2]; then
+echo
+"Usage: $0 /path/to/input_dir /path/to/output_dir [--max_depth N]"
+exit
+1
+fi
+
+input_dir = "$1"
+output_dir = "$2"
+max_depth = ""
+
+
+if ["$#" - eq 4] & & ["$3" == "--max_depth"]; then
+max_depth = "$4"
+fi
+
+if [ ! -d "$input_dir"]; then
+echo
+"Input directory does not exist: $input_dir"
+exit
+1
+fi
+
+mkdir - p
+"$output_dir"
+
+
+find_command = "find \"$input_dir\" -type f"
+if [-n "$max_depth"]; then
+find_command = "find \"$input_dir\" -maxdepth $max_depth -type f"
+fi
+
+
+eval
+"$find_command" |
+while read - r file; do
+base_name =$(basename "$file")
+target = "$output_dir/$base_name"
+
+
+counter = 1
+while [-e "$target"]; do
+target = "$output_dir/${base_name%.*}_$counter.${base_name##*.}"
+counter =$((counter + 1))
+done
+
+cp
+"$file" "$target"
+done
